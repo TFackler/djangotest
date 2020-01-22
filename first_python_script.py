9bd37c8fe4479657e30d98128e36658a3b114550
TFackler <Fackler.T@gmx.de>
2020-01-22 20:42:08 +0100

    init

---
+++ first_python_script.py
@@ -0,0 +1,10 @@
+print('Hello World')
+def hi():
+    print('Hello there!')
+    print('\t\tGeneral Kenobi =w=')
+def hi(name):
+    print('Hello there!')
+    print('\t\t' + name + '!')
+
+hi('General Kenobi')
+hi('Max')
